851cbaea42500f99ec39053a101966041de6d1a4
Kristsina Suvorava <krisvit1999@gmail.com>
2023-01-12 14:07:05 +0200

    HelloScreen from Onboarding was finished

--- Noontime List/Noontime List/Screens/Onboarding/Hello/HelloViewController.swift
+++ Noontime List/Noontime List/Screens/Onboarding/Hello/HelloViewController.swift
@@ -9,8 +9,29 @@ import UIKit
 
 class HelloViewController: UIViewController {
 
+    @IBOutlet weak var skipButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpButton()
+    }
+
+    @IBAction private func tappedSkipButton(_ sender:  UIButton) {
+        let controller = viewController(storyboardName: "PlannerHomeScreen", identifier: "PlannerHomeScreen")
+        navigationController?.pushViewController(controller, animated: true)
+    }
+
+    @IBAction private func tappedNextButton(_ sender:  UIButton) {
+        let controller = viewController(storyboardName: "PlannerScreen", identifier: "PlannerScreen")
+        navigationController?.pushViewController(controller, animated: true)
+    }
+
+    private func setUpButton() {
+        skipButton.layer.borderWidth = 1
+        skipButton.layer.borderColor = UIColor.black.cgColor
+        skipButton.layer.cornerRadius = 10
+        nextButton.layer.cornerRadius = 10
     }
 }
 
