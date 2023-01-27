cdade51bd1ca65a50d4ab0521dbfc32e64c264a7
Kristsina Suvorava <krisvit1999@gmail.com>
2023-01-27 14:17:50 +0200

    Pod installed

---
+++ Noontime List/Noontime List/Model/Day.swift
@@ -0,0 +1,16 @@
+//
+//  Day.swift
+//  Noontime List
+//
+//  Created by Vlad Birukov on 27.01.2023.
+//
+
+import UIKit
+
+struct Day {
+
+  let date: Date
+  let number: String
+  let isSelected: Bool
+  let isWithinDisplayedMonth: Bool
+}
--- Noontime List/Noontime List/Screens/HomePlanner/HomePlannerViewController.swift
+++ Noontime List/Noontime List/Screens/HomePlanner/HomePlannerViewController.swift
@@ -9,7 +9,21 @@ import UIKit
 
 class HomePlannerViewController: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpLabel()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+        view.layoutIfNeeded()
+    }
+
+    private func setUpLabel() {
+        let date = Date()
+        dateLabel.text = date.getStringFromDate(format: "MMM d")
     }
 }
