c6a6b8cd9f35a048496af039cddbe70e96e5dbf8
Kristsina Suvorava <krisvit1999@gmail.com>
2023-02-03 13:40:33 +0200

    HomePlannerScreen was updated

--- Noontime List/Noontime List/Screens/HomePlanner/HomePlannerViewController.swift
+++ Noontime List/Noontime List/Screens/HomePlanner/HomePlannerViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import FSCalendar
+
 
 class HomePlannerViewController: UIViewController {
 
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var calendarButton: UIButton!
+    @IBOutlet weak var calendar: FSCalendar!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpLabel()
+        setUpCalendar()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -22,8 +27,39 @@ class HomePlannerViewController: UIViewController {
         view.layoutIfNeeded()
     }
 
+    @IBAction private func tappedCalendarButton(_ sender: UIButton) {
+        if calendar.scope == FSCalendarScope.month {
+            calendar.scope = .week
+            calendarButton.setImage(UIImage(named: "monthCalendar"), for: .normal)
+        } else {
+            calendar.scope = .month
+            calendarButton.setImage(UIImage(named: "weekCalendar"), for: .normal)
+        }
+        calendar.collectionViewLayout.invalidateLayout()
+    }
+
     private func setUpLabel() {
         let date = Date()
         dateLabel.text = date.getStringFromDate(format: "MMM d")
     }
+
+    private func setUpCalendar() {
+        calendar.appearance.titleFont = UIFont(name: "Times New Roman", size: 17)
+        calendar.appearance.weekdayFont = UIFont(name: "Times New Roman", size: 17)
+        calendar.appearance.headerTitleFont = UIFont(name: "Times New Roman", size: 18)
+
+        calendar.today = nil
+
+        calendar.appearance.headerTitleColor = .black
+        calendar.appearance.weekdayTextColor = UIColor(red: 96/255, green: 111/255, blue: 130/255, alpha: 1.00)
+        calendar.appearance.titleWeekendColor = UIColor(red: 96/255, green: 111/255, blue: 130/255, alpha: 1.00)
+        calendar.appearance.selectionColor = UIColor(red: 250/255, green: 26/255, blue: 142/255, alpha: 1.00)
+
+        calendar.placeholderType = .none
+        calendar.appearance.headerMinimumDissolvedAlpha = 0
+
+        calendar.firstWeekday = 2
+    }
 }
+
+
